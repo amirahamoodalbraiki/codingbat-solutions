1e24243a5e4ba21df265ce54e72a0fed15622407
Amira Albraiki <amira@example.com>
2025-07-27 15:54:53 +0400

    Add solution for warmup2-frontTimes

--- warmup2/StringTimes.java
+++ warmup2/StringTimes.java
@@ -1,9 +1,5 @@
+git checkout main
+git pull origin main
 
-  public String stringTimes(String str, int n) {
-  String result = "";
-  for (int i=0; i<n; i++) {
-    result = result + str;
-  }
-  return result;
-}
-  
+git merge warmup2-stringTimes
+git push origin main
