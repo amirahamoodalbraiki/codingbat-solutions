d62a47393effcee3a00b00e8689415e6a7b3a1bd
Amira Albraiki <amira@example.com>
2025-07-27 18:50:36 +0400

    Add solution for string3-countTriple

---
+++ String-3/CountTriple.java
@@ -0,0 +1,11 @@
+
+public int countTriple(String str) {
+  int c = 0;
+  
+  for(int i = 0; i < str.length() - 2; i++) {
+    if(str.charAt(i) == str.charAt(i+1) && str.charAt(i+1) == str.charAt(i+2)){
+      c =c+1;
+    }
+  }
+  return c;  
+}
