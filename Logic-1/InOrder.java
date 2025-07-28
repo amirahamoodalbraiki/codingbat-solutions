1d62ca52750998a2804cdcafba2763f10703d10b
Amira Albraiki <amira@example.com>
2025-07-28 08:35:52 +0400

    Add solution for logic1-inOrder

---
+++ Logic-1/InOrder.java
@@ -0,0 +1,11 @@
+
+public boolean inOrder(int a, int b, int c, boolean bOk) {
+    if(bOk && c > b){
+      return true;
+    } else if(b > a && c > b){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
