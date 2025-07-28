26bcc6f32f1ff1a8fb019bf71e51112349009e7d
Amira Albraiki <amira@example.com>
2025-07-28 10:16:08 +0400

    Add solution for ap1-wordsFront

---
+++ AP-1/WordsFront.java
@@ -0,0 +1,9 @@
+
+public String[] wordsFront(String[] words, int n) {
+    String[] array = new String[n];
+    
+    for(int i = 0; i < array.length; i++)
+        array[i] = words[i];
+              
+    return array;
+}
