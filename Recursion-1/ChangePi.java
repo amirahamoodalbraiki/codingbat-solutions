864d81f83c1960a29554476fd6d873c5321aee73
Amira Albraiki <amira@example.com>
2025-07-28 09:26:43 +0400

    Add solution for recursion1-changePi

---
+++ Recursion-1/ChangePi.java
@@ -0,0 +1,11 @@
+
+public String changePi(String str) {
+  
+  if(str.length() <= 1){
+    return str;
+  }
+  if(str.substring(str.length()-2).equals("pi")){
+    return  changePi(str.substring(0,str.length()-2)) + "3.14";
+  }
+  return changePi(str.substring(0,str.length()-1)) + str.charAt(str.length()-1);
+}
