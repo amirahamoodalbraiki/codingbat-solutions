5eac3e997ba7073aedadcb9caf7898da19f2b886
Amira Albraiki <amira@example.com>
2025-07-28 08:31:58 +0400

    Add solution for logic1-fizzString

---
+++ Logic-1/FizzString.java
@@ -0,0 +1,13 @@
+
+public String fizzString(String str) {
+    if(str.startsWith("f") && str.endsWith("b")){
+      return "FizzBuzz";
+    } else if(str.startsWith("f")){
+      return "Fizz";
+    } else if(str.endsWith("b")){
+      return "Buzz";
+    } else {
+      return str;
+    }
+  }
+  
